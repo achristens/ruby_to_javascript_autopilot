05e3bcbfc3a004b3b2244827b6303eb09bcdeb7e
Abby Christens <achristens@gmail.com>
2017-11-07 14:40:53 -0500

    addCar and getNewCar functions

---
+++ autopilot.js
@@ -0,0 +1,22 @@
+var getNewCar =  {
+  "city":       "Toronto",
+  "passengers": 0,
+  "gas":        100
+};
+var abbysCar = getNewCar
+var arisCar = getNewCar
+
+var cars = [];
+
+function addCar(new_car){
+  cars.push(new_car);
+}
+
+
+//
+// console.log("Abby is in " + abbysCar.city);
+// console.log("Ari has " + arisCar.passengers + " passengers");
+// arisCar.passengers += 2
+// console.log("Ari has " + arisCar.passengers + " passengers");
+// arisCar.passengers += 2
+// console.log("Ari has " + arisCar.passengers + " passengers");
